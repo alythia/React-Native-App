71b481fd9430f50ca8e6a4afa10ee82d140fed46
Matt Ferguson <mdfergus@gmail.com>
2018-06-26 11:52:52 -0400

    feature(native) apps fully builds, added QR reader, added biomet auth, added loading screen

--- Alythia/App.js
+++ Alythia/App.js
@@ -1,13 +1,35 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View, Image } from "react-native";
 
 export default class App extends React.Component {
+  componentDidMount = () => {
+    setTimeout(() => this.handleBiometrics(), 3000);
+  };
+
+  handleBiometrics = async () => {
+    try {
+      const res = await Expo.Fingerprint.isEnrolledAsync();
+      if (res === true) {
+        const fingerPrint = await Expo.Fingerprint.authenticateAsync();
+        console.log(fingerPrint);
+        // if (fingerPrint.error === 'user_fallback') {
+        //   // this is where we need to ask user for PIN or Password
+        // }
+      }
+    } catch (e) {
+      console.log("error is: ", e);
+    }
+  };
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        <View style={styles.padding} />
+        <Image
+          source={require("./public/Logo-funky.png")}
+          style={styles.image}
+        />
+        <View style={styles.padding} />
       </View>
     );
   }
@@ -16,8 +38,15 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#061A40",
+    alignItems: "center",
+    justifyContent: "center"
   },
+  image: {
+    flex: 1,
+    maxWidth: 300
+  },
+  padding: {
+    flex: 4
+  }
 });
---
+++ Alythia/components/scanner.js
@@ -0,0 +1,39 @@
+import React, { Component } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { BarCodeScanner, Permissions } from "expo";
+
+export class Scanner extends Component {
+  state = {
+    hasCameraPermission: null
+  };
+
+  async componentWillMount() {
+    const { status } = await Permissions.askAsync(Permissions.CAMERA);
+    this.setState({ hasCameraPermission: status === "granted" });
+  }
+
+  render() {
+    const { hasCameraPermission } = this.state;
+
+    if (hasCameraPermission === null) {
+      return <Text>Requesting for camera permission</Text>;
+    } else if (hasCameraPermission === false) {
+      return <Text>No access to camera</Text>;
+    } else {
+      return (
+        <View style={{ flex: 1 }}>
+          <BarCodeScanner
+            onBarCodeRead={this._handleBarCodeRead}
+            style={StyleSheet.absoluteFill}
+          />
+        </View>
+      );
+    }
+  }
+
+  _handleBarCodeRead = ({ type, data }) => {
+    console.log(`This is your info: ${data}`);
+  };
+}
+
+export default Scanner;

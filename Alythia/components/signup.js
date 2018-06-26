cb3561693063a7f22e681c77a5d40f6ae3c640af
Matt Ferguson <mdfergus@gmail.com>
2018-06-26 19:16:17 -0400

    feature(forms) added TouchableWithoutFeedback

--- Alythia/components/signup.js
+++ Alythia/components/signup.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { View } from "react-native";
+import { View, Keyboard, TouchableWithoutFeedback } from "react-native";
 import {
   FormLabel,
   FormInput,
@@ -24,71 +24,77 @@ export class Signup extends Component {
   render() {
     console.log(this.state.pin);
     return (
-      <View
-        style={{
-          flex: 1,
-          flexDirection: "column",
-          justifyContent: "center"
-        }}
-      >
-        <View>
-          <Text
-            h2
-            style={{
-              textAlign: "center",
-              color: "#061A40",
-              fontWeight: "bold"
-            }}
-          >
-            SIGN UP
-          </Text>
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
+        <View
+          style={{
+            flex: 1,
+            flexDirection: "column",
+            justifyContent: "center"
+          }}
+        >
+          <View>
+            <Text
+              h2
+              style={{
+                textAlign: "center",
+                color: "#061A40",
+                fontWeight: "bold"
+              }}
+            >
+              SIGN UP
+            </Text>
+          </View>
+          <View style={{ height: 20 }} />
+          <View>
+            <FormLabel style={{ marginTop: "15px" }}>Email</FormLabel>
+            <FormInput
+              placeholder="Please enter your email..."
+              value={this.state.email}
+              onChangeText={text => this.setState({ email: text })}
+              keyboardType="email-address"
+            />
+            <FormValidationMessage>
+              This field is required
+            </FormValidationMessage>
+          </View>
+          <View style={{ height: 20 }} />
+          <View>
+            <FormLabel style={{ marginTop: "15px" }}>6-Digit Pin</FormLabel>
+            <FormInput
+              placeholder="Please select a secure 6-digit pin..."
+              value={this.state.pin}
+              onChangeText={text => this.setState({ pin: text })}
+              keyboardType="numeric"
+              maxLength={6}
+              secureTextEntry={true}
+            />
+            <FormValidationMessage>
+              This field is required
+            </FormValidationMessage>
+          </View>
+          <View style={{ height: 50 }} />
+          <View>
+            <Button
+              raised
+              icon={{ name: "check" }}
+              backgroundColor="#061A40"
+              title="CREATE ACCONT"
+              onPress={this.handleSubmit}
+            />
+          </View>
+          <View style={{ height: 10 }} />
+          <View>
+            <Button
+              raised
+              backgroundColor="#006DB6"
+              icon={{ name: "md-arrow-round-back", type: "ionicon" }}
+              title="OR GO BACK TO LOG IN"
+              onPress={() => this.props.navigation.navigate("Login")}
+            />
+          </View>
+          <View style={{ height: 80 }} />
         </View>
-        <View style={{ height: 20 }} />
-        <View>
-          <FormLabel style={{ marginTop: "15px" }}>Email</FormLabel>
-          <FormInput
-            placeholder="Please enter your email..."
-            value={this.state.email}
-            onChangeText={text => this.setState({ email: text })}
-            keyboardType="email-address"
-          />
-          <FormValidationMessage>This field is required</FormValidationMessage>
-        </View>
-        <View style={{ height: 20 }} />
-        <View>
-          <FormLabel style={{ marginTop: "15px" }}>6-Digit Pin</FormLabel>
-          <FormInput
-            placeholder="Please select a secure 6-digit pin..."
-            value={this.state.pin}
-            onChangeText={text => this.setState({ pin: text })}
-            keyboardType="numeric"
-            maxLength={6}
-            secureTextEntry={true}
-          />
-          <FormValidationMessage>This field is required</FormValidationMessage>
-        </View>
-        <View style={{ height: 50 }} />
-        <View>
-          <Button
-            raised
-            icon={{ name: "check" }}
-            backgroundColor="#061A40"
-            title="CREATE ACCONT"
-            onPress={this.handleSubmit}
-          />
-        </View>
-        <View style={{ height: 10 }} />
-        <View>
-          <Button
-            raised
-            backgroundColor="#006DB6"
-            icon={{ name: "md-arrow-round-back", type: "ionicon" }}
-            title="OR GO BACK TO LOG IN"
-            onPress={() => this.props.navigation.navigate("Login")}
-          />
-        </View>
-        <View style={{ height: 80 }} />
-      </View>
+      </TouchableWithoutFeedback>
     );
   }
 }

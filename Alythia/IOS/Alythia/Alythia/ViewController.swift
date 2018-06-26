fb6692a870dbdb079add36a7ce809a6759726acf
Matt Ferguson <mdfergus@gmail.com>
2018-06-25 21:03:31 -0400

    feature(native) setup

---
+++ Alythia/IOS/Alythia/Alythia/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Alythia
+//
+//  Created by Matt Ferguson on 6/25/18.
+//  Copyright © 2018 Matt Ferguson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

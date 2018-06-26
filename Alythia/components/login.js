0e0b54e4e4c0762176b2c959a86994958cbf0a50
Matt Ferguson <mdfergus@gmail.com>
2018-06-26 18:49:02 -0400

    feature(forms) added pin and email keyboard type, set pin length to 6, and hid pin characters in the form

--- Alythia/components/login.js
+++ Alythia/components/login.js
@@ -1,66 +1,72 @@
-import React, { Component } from 'react'
-import { View } from 'react-native'
+import React, { Component } from "react";
+import { View } from "react-native";
 import {
   FormLabel,
   FormInput,
   FormValidationMessage,
   Button,
-  Text,
-} from 'react-native-elements'
+  Text
+} from "react-native-elements";
 
 import {
   retrieveUserEmail,
-  retrieveAndValidateUserPin,
-} from '../utils/secure-store'
+  retrieveAndValidateUserPin
+} from "../utils/secure-store";
 
 export class Login extends Component {
   state = {
-    email: '',
-    pin: '',
-  }
+    email: "",
+    pin: ""
+  };
 
   handleSubmit = () => {
-    const userEmail = retrieveUserEmail()
-    const successfulSignIn = retrieveAndValidateUserPin()
-    console.log(`User email: ${userEmail}`, `Success? ${successfulSignIn}`)
-  }
+    const userEmail = retrieveUserEmail();
+    const successfulSignIn = retrieveAndValidateUserPin();
+    console.log(`User email: ${userEmail}`, `Success? ${successfulSignIn}`);
+  };
 
   render() {
     return (
       <View
         style={{
           flex: 1,
-          flexDirection: 'column',
-          justifyContent: 'center',
-        }}>
+          flexDirection: "column",
+          justifyContent: "center"
+        }}
+      >
         <View>
           <Text
             h2
             style={{
-              textAlign: 'center',
-              color: '#061A40',
-              fontWeight: 'bold',
-            }}>
+              textAlign: "center",
+              color: "#061A40",
+              fontWeight: "bold"
+            }}
+          >
             LOG IN
           </Text>
         </View>
         <View style={{ height: 20 }} />
         <View>
-          <FormLabel style={{ marginTop: '15px' }}>Email</FormLabel>
+          <FormLabel style={{ marginTop: "15px" }}>Email</FormLabel>
           <FormInput
             placeholder="Please enter your email..."
             value={this.state.email}
             onChangeText={text => this.setState({ email: text })}
+            keyboardType="email-address"
           />
           <FormValidationMessage>This field is required</FormValidationMessage>
         </View>
         <View style={{ height: 20 }} />
         <View>
-          <FormLabel style={{ marginTop: '15px' }}>6-Digit Pin</FormLabel>
+          <FormLabel style={{ marginTop: "15px" }}>6-Digit Pin</FormLabel>
           <FormInput
             placeholder="Please input your 6-digit pin..."
             value={this.state.pin}
             onChangeText={text => this.setState({ pin: text })}
+            keyboardType="numeric"
+            maxLength={6}
+            secureTextEntry={true}
           />
           <FormValidationMessage>This field is required</FormValidationMessage>
         </View>
@@ -68,10 +74,10 @@ export class Login extends Component {
         <View>
           <Button
             raised
-            icon={{ name: 'check' }}
+            icon={{ name: "check" }}
             backgroundColor="#061A40"
             title="LOG IN"
-            onClick={this.handleSubmit}
+            onPress={this.handleSubmit}
           />
         </View>
         <View style={{ height: 10 }} />
@@ -79,15 +85,15 @@ export class Login extends Component {
           <Button
             raised
             backgroundColor="#006DB6"
-            icon={{ name: 'add-user', type: 'entypo' }}
+            icon={{ name: "add-user", type: "entypo" }}
             title="OR CREATE ACCOUNT"
-            onPress={() => this.props.navigation.navigate('Signup')}
+            onPress={() => this.props.navigation.navigate("Signup")}
           />
         </View>
         <View style={{ height: 80 }} />
       </View>
-    )
+    );
   }
 }
 
-export default Login
+export default Login;
--- Alythia/components/signup.js
+++ Alythia/components/signup.js
@@ -1,62 +1,69 @@
-import React, { Component } from 'react'
-import { View } from 'react-native'
+import React, { Component } from "react";
+import { View } from "react-native";
 import {
   FormLabel,
   FormInput,
   FormValidationMessage,
   Button,
-  Text,
-} from 'react-native-elements'
+  Text
+} from "react-native-elements";
 
-import { setUserEmail, setUserPin } from '../utils/secure-store'
+import { setUserEmail, setUserPin } from "../utils/secure-store";
 
 export class Signup extends Component {
   state = {
-    email: '',
-    pin: '',
-  }
+    email: "",
+    pin: ""
+  };
 
   handleSubmit = () => {
-    setUserEmail(this.state.email)
-    setUserPin(this.state.pin)
-  }
+    setUserEmail(this.state.email);
+    setUserPin(this.state.pin);
+  };
 
   render() {
+    console.log(this.state.pin);
     return (
       <View
         style={{
           flex: 1,
-          flexDirection: 'column',
-          justifyContent: 'center',
-        }}>
+          flexDirection: "column",
+          justifyContent: "center"
+        }}
+      >
         <View>
           <Text
             h2
             style={{
-              textAlign: 'center',
-              color: '#061A40',
-              fontWeight: 'bold',
-            }}>
+              textAlign: "center",
+              color: "#061A40",
+              fontWeight: "bold"
+            }}
+          >
             SIGN UP
           </Text>
         </View>
         <View style={{ height: 20 }} />
         <View>
-          <FormLabel style={{ marginTop: '15px' }}>Email</FormLabel>
+          <FormLabel style={{ marginTop: "15px" }}>Email</FormLabel>
           <FormInput
             placeholder="Please enter your email..."
             value={this.state.email}
             onChangeText={text => this.setState({ email: text })}
+            keyboardType="email-address"
           />
           <FormValidationMessage>This field is required</FormValidationMessage>
         </View>
         <View style={{ height: 20 }} />
         <View>
-          <FormLabel style={{ marginTop: '15px' }}>6-Digit Pin</FormLabel>
+          <FormLabel style={{ marginTop: "15px" }}>6-Digit Pin</FormLabel>
           <FormInput
             placeholder="Please select a secure 6-digit pin..."
             value={this.state.pin}
             onChangeText={text => this.setState({ pin: text })}
+            keyboardType="numeric"
+            maxLength={6}
+            secureTextEntry={true}
           />
           <FormValidationMessage>This field is required</FormValidationMessage>
         </View>
@@ -64,7 +71,7 @@ export class Signup extends Component {
         <View>
           <Button
             raised
-            icon={{ name: 'check' }}
+            icon={{ name: "check" }}
             backgroundColor="#061A40"
             title="CREATE ACCONT"
             onPress={this.handleSubmit}
@@ -75,15 +82,15 @@ export class Signup extends Component {
           <Button
             raised
             backgroundColor="#006DB6"
-            icon={{ name: 'md-arrow-round-back', type: 'ionicon' }}
+            icon={{ name: "md-arrow-round-back", type: "ionicon" }}
             title="OR GO BACK TO LOG IN"
-            onPress={() => this.props.navigation.navigate('Login')}
+            onPress={() => this.props.navigation.navigate("Login")}
           />
         </View>
         <View style={{ height: 80 }} />
       </View>
-    )
+    );
   }
 }
 
-export default Signup
+export default Signup;

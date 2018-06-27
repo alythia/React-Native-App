192d5b760834b9328d82af6e03dd27182ce928fb
Dave Trabka <dtrabka@gmail.com>
2018-06-26 21:56:25 -0400

    feat(account-created): added quick interim page that shows successful account creation before redirecting to scanner

--- Alythia/App.js
+++ Alythia/App.js
@@ -1,9 +1,9 @@
 import React from 'react'
-import { StyleSheet, Text, View, Image } from 'react-native'
 import Loading from './components/loading'
 import Scanner from './components/scanner'
 import Login from './components/login'
 import Signup from './components/signup'
+import AccountCreated from './components/account-created'
 import { createStackNavigator } from 'react-navigation'
 
 const App = createStackNavigator(
@@ -11,6 +11,7 @@ const App = createStackNavigator(
     Loading: { screen: Loading },
     Login: { screen: Login },
     Signup: { screen: Signup },
+    AccountCreated: { screen: AccountCreated },
     Scanner: { screen: Scanner },
   },
   {
--- Alythia/components/biometrics.js
+++ Alythia/components/biometrics.js
@@ -1,30 +1,30 @@
-import React, { Component } from "react";
+import React, { Component } from 'react'
 
-export class Biometrics extends Component {
+class Biometrics extends Component {
   componentDidMount = () => {
-    setTimeout(() => this.handleBiometrics(), 3000);
-  };
+    setTimeout(() => this.handleBiometrics(), 3000)
+  }
 
   handleBiometrics = async () => {
     try {
-      const res = await Expo.Fingerprint.isEnrolledAsync();
+      const res = await Expo.Fingerprint.isEnrolledAsync()
       if (res === true) {
-        const fingerPrint = await Expo.Fingerprint.authenticateAsync();
+        const fingerPrint = await Expo.Fingerprint.authenticateAsync()
         if (fingerPrint.success) {
-          this.props.navigation.navigate("Scanner");
+          this.props.navigation.navigate('Scanner')
         }
         // if (fingerPrint.error === 'user_fallback') {
         //   // this is where we need to ask user for PIN or Password
         // }
       }
     } catch (e) {
-      console.log("error is: ", e);
+      console.log('error is: ', e)
     }
-  };
+  }
 
   render() {
-    return <div />;
+    return <div />
   }
 }
 
-export default Biometrics;
+export default Biometrics

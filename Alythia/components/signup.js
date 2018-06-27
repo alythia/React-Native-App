58169d3d3671be2c3ec8a73f7caf0939df0c0329
Dave Trabka <dtrabka@gmail.com>
2018-06-26 21:27:59 -0400

    feat(forms): just tweaked super minor button labels cause Im OCD sometimes

--- Alythia/components/signup.js
+++ Alythia/components/signup.js
@@ -1,26 +1,26 @@
-import React, { Component } from "react";
-import { View, Keyboard, TouchableWithoutFeedback } from "react-native";
+import React, { Component } from 'react'
+import { View, Keyboard, TouchableWithoutFeedback } from 'react-native'
 import {
   FormLabel,
   FormInput,
   FormValidationMessage,
   Button,
-  Text
-} from "react-native-elements";
+  Text,
+} from 'react-native-elements'
 
-import { setUserEmail, setUserPin } from "../utils/secure-store";
+import { setUserEmail, setUserPin } from '../utils/secure-store'
 
 export class Signup extends Component {
   state = {
-    email: "",
-    pin: ""
-  };
+    email: '',
+    pin: '',
+  }
 
   handleSubmit = () => {
-    console.log("New User: ", this.state);
-    setUserEmail(this.state.email);
-    setUserPin(this.state.pin);
-  };
+    console.log('New User: ', this.state)
+    setUserEmail(this.state.email)
+    setUserPin(this.state.pin)
+  }
 
   render() {
     return (
@@ -28,25 +28,23 @@ export class Signup extends Component {
         <View
           style={{
             flex: 1,
-            flexDirection: "column",
-            justifyContent: "center"
-          }}
-        >
+            flexDirection: 'column',
+            justifyContent: 'center',
+          }}>
           <View>
             <Text
               h2
               style={{
-                textAlign: "center",
-                color: "#061A40",
-                fontWeight: "bold"
-              }}
-            >
+                textAlign: 'center',
+                color: '#061A40',
+                fontWeight: 'bold',
+              }}>
               SIGN UP
             </Text>
           </View>
           <View style={{ height: 20 }} />
           <View>
-            <FormLabel style={{ marginTop: "15px" }}>Email</FormLabel>
+            <FormLabel style={{ marginTop: '15px' }}>Email</FormLabel>
             <FormInput
               placeholder="Please enter your email..."
               value={this.state.email}
@@ -59,7 +57,7 @@ export class Signup extends Component {
           </View>
           <View style={{ height: 20 }} />
           <View>
-            <FormLabel style={{ marginTop: "15px" }}>6-Digit Pin</FormLabel>
+            <FormLabel style={{ marginTop: '15px' }}>6-Digit Pin</FormLabel>
             <FormInput
               placeholder="Please select a secure 6-digit pin..."
               value={this.state.pin}
@@ -76,9 +74,9 @@ export class Signup extends Component {
           <View>
             <Button
               raised
-              icon={{ name: "check" }}
+              icon={{ name: 'check' }}
               backgroundColor="#061A40"
-              title="CREATE ACCONT"
+              title="CREATE MY ACCOUNT"
               onPress={this.handleSubmit}
             />
           </View>
@@ -87,16 +85,16 @@ export class Signup extends Component {
             <Button
               raised
               backgroundColor="#006DB6"
-              icon={{ name: "md-arrow-round-back", type: "ionicon" }}
-              title="OR GO BACK TO LOG IN"
-              onPress={() => this.props.navigation.navigate("Login")}
+              icon={{ name: 'md-arrow-round-back', type: 'ionicon' }}
+              title="BACK TO LOG IN"
+              onPress={() => this.props.navigation.navigate('Login')}
             />
           </View>
           <View style={{ height: 80 }} />
         </View>
       </TouchableWithoutFeedback>
-    );
+    )
   }
 }
 
-export default Signup;
+export default Signup

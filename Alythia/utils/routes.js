f2994d5285c4a2084668dc0713151ede1c2aa2d9
Matt Ferguson <mdfergus@gmail.com>
2018-07-05 10:38:39 -0400

    making changes to Alythia API root

--- Alythia/utils/routes.js
+++ Alythia/utils/routes.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // my home remote localhost to use my own iPhone (not simulator)
 // const rootDomain = 'http://10.0.1.15:8080'
-const rootDomain = "http://172.16.23.149:8080"; //'http://localhost:8080' // TODO: THIS WILL HAVE TO BE A HTTPS
+const rootDomain = "http://alythia.herokuapp.com"; //'http://localhost:8080' // TODO: THIS WILL HAVE TO BE A HTTPS
 // const rootDomain = 'http://172.16.23.149:8080'
 
 export const createUser = async (userEmail, userUUID) => {
@@ -30,10 +30,10 @@ export const transferData = async (QRCodeData, userEmail, userUUID) => {
     };
     console.log("POSTed to Alythia", payload);
     const { data } = await axios.post(
-      rootDomain + "/api/users/verify/",
+      "https://alythia.herokuapp.com/api/users/verify/",
       payload
     );
-
+    console.log("Res from axios call: ", data);
     return data;
   } catch (error) {
     console.error(error);
